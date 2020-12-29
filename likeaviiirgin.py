cc33a7b07f513d9df399ee28722d50a7da825c16
Tahri Oussama <tahrioussama3@gmail.com>
2020-12-29 21:52:26 +0100

    test oussama

--- likeaviiirgin.py
+++ likeaviiirgin.py
@@ -5,6 +5,7 @@ import io
 import matplotlib.pyplot as plt
 from math import atan, sqrt, acos, asin
 #comment
+#test
 class Point:
     Lat, Long, Az, Beta1, Beta0, W2, A1, B1, Sigma, alphaE, Sigma1, Sm, Dsigma, Sigmaf, x, y, z = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
