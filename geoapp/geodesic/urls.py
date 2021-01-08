c24e0a2b5b31ad5a980ca02b0a63ecb277dbd2b7
molaiea <nafissaatlagh180@gmail.com>
2021-01-08 12:55:28 +0100

    adding form

--- geoapp/geodesic/urls.py
+++ geoapp/geodesic/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('/direct', views.direct, name="direct"),
-    path('/inverse', views.inverse, name="inverse"),
+    path('direct', views.direct, name="direct"),
+    path('inverse', views.inverse, name="inverse"),
 ]
