efd1e3e7d8bfeba8335b500591c51af28f6d0f92
molaiea <nafissaatlagh180@gmail.com>
2021-01-08 11:35:42 +0100

    adding starter homepage

---
+++ geoapp/geodesic/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('/direct', views.direct, name="direct"),
+    path('/inverse', views.inverse, name="inverse"),
+]
--- geoapp/geodesic/views.py
+++ geoapp/geodesic/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request) :
+    return render(request, 'home.html')
+
+
+def inverse(request) :
+    return render(request, 'inverse.html')
+
+def direct(request) :
+    return render(request, 'direct.html')
